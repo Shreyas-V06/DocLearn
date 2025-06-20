cfb9a3db78af02a6a4dfbcfbcf79d7a50c81ffe2
Shreyas-V06 <shreyasvakkiyel@gmail.com>
2025-06-20 21:11:21 +0530

    fix: CORS fix 2

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://doclearn-sv.vercel.app/"],
+    allow_origins=["https://doclearn-sv.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
